e8a116bdd245f7ce86eb765fde6329c2d8ba46b6
sriramede9 <only4programming03@gmail.com>
2026-07-12 12:15:14 -0400

    python: for and list

--- hello.py
+++ hello.py
@@ -1,58 +1,17 @@
-# variables
-name = "Alice"
-age = 30
-height = 5.8
-isStudent = False
-fee = 900.50
+for i in range(10):
+    print(i ** i)
 
-welcome_banner = """
-Welcome to          the 
-                            lazy world of 
-            learning python
-                                you should care or someone has the skills you always wanted!!!!                
+# count by 2    
+for i in range(0, 20, 2):
+    print(i)
 
-"""
+# understand list
 
-print("Name:",name, ",Age:", age, ",Height:", height,",Is Student:", isStudent)
+my_list = ["Sri","Potato","Tomato",10,True,False,33.33]
+print(my_list[-1])
+print(my_list[-3])
+print(my_list.append("Car"))
+print("*"*10)
 
-print ("Age * Height =","-"*12, age ** 2 , "_"*25)
-
-print(welcome_banner)
-if not isStudent or not fee < 1000:
-    print("Hello ",name)
-else:
-    print ("Hey I am Bart Simpson, who the hell are you??")
-
-#  Let's do some exception handling
-  
-total = 500
-
-try:
-    subtotal = total/"abc"
-except (ZeroDivisionError,TypeError) as z:
-    print("do not divide by zero hero!!!") 
-finally:
-    print("we are over the try catch subtotal block")
-
-numbers = [1,2,3,4,5]
-
-try:
-    print("well this is the lucky number",  numbers[5])
-except (IndexError) as e:
-    print("well ain't that lucky ",e)
-finally:
-    print("we are done with lucky numbers")
-
-student = {
-    "name":"Sri",
-    "age" : 30,
-    "isStudent":False
-}            
-
-try:
-    open("student.txt")
-    print("salary of :" , student["name"] , "is",student["salary"])
-except (KeyError,FileNotFoundError) as e:
-    print(f"invalid Key or file not found:{e}")
-finally:
-    print("hey Done with Student",student)        
+for item in my_list:
+    print(item)
