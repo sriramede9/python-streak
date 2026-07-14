315af805f8df0a5cafdbf6aefe0d018b5643f1e0
sriramede9 <only4programming03@gmail.com>
2026-07-13 21:06:32 -0400

    python: functions with return types and exceptions

--- hello.py
+++ hello.py
@@ -1,17 +1,45 @@
-student = {
-    "name": "Sri",
-    "age":10,
-    "is_Student":False
-}
+# functions
 
-print(student["age"].upper())
 
-# sets
+def greet():
+    print("Welcome Function")
 
-numbers = {1, 2, 3, 4, 5}
-random_numbers = {3, 4, 5, 6, 7,7,8,8,8,8,9,9,9,9,9}
 
-print(random_numbers)
+greet()
 
-# tuples
-colors = ("red", "green", "blue")
+# how to accept variables?
+
+
+def greetPerson(name: str):
+    print("Hey Buddy what's up", name.upper())
+    print(f"Hey {name.upper()} what's ups")
+
+
+greetPerson("Mikes")
+
+
+def check_weather(data: int):
+    if data > 25:
+        print("Hot stay home")
+    else:
+        print("Go out you monster")
+
+
+check_weather(18)
+
+
+def check_weather2(data: int) -> str:
+    if not isinstance(data, int):
+           raise TypeError("data must be an integer")
+    if data > 25:
+        return "Hot stay home"
+    else:
+        return "Go out you monster"
+
+try:
+    decision = check_weather2("abc")
+except TypeError as e:
+    print(f"Type error {e}")
+
+
+print(f"You better do this: {check_weather2(18)}")
