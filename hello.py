3f383ef5c116505675f4f46e426766947c6d5087
sriramede9 <only4programming03@gmail.com>
2026-07-16 11:31:47 -0400

    python: streaming in list vs numpy

--- hello.py
+++ hello.py
@@ -15,25 +15,25 @@ print(arr[::2])  # alternative element [1,3,5,76]
 print(arr[5:,])  # skip first 5 elements [6,76,7]
 
 # how to make it n dimensional array
-new_arr = np.array([1,2,3,4,5],ndmin=5) # crates a 5D array [[[[[1,2,3,4,5]]]]]
-#NumPy won't remove dimensions. It only adds singleton (size 1) dimensions if needed
+new_arr = np.array([1, 2, 3, 4, 5], ndmin=5)  # crates a 5D array [[[[[1,2,3,4,5]]]]]
+# NumPy won't remove dimensions. It only adds singleton (size 1) dimensions if needed
 
-# understand data types 
+# understand data types
 
-int_arr = np.array([1,2,3,4,5,6],dtype="i")
+int_arr = np.array([1, 2, 3, 4, 5, 6], dtype="i")
 print(type(int_arr))
 
-arr = np.array(['apple', 'banana', 'cherry'])
+arr = np.array(["apple", "banana", "cherry"])
 
 print(arr.dtype)
 
-arr = np.array([1, 2, 3, 4], dtype='U')
+arr = np.array([1, 2, 3, 4], dtype="U")
 
 print(arr)
 print(arr.dtype)
 
 try:
-    arr = np.array(['a', '2', '3'], dtype='i')
+    arr = np.array(["a", "2", "3"], dtype="i")
 except ValueError as e:
     print("Error:", e)
 
@@ -41,5 +41,36 @@ except ValueError as e:
 
 double_arr = np.array([1.1, 2.2, 3.3, 4.4])
 print(double_arr.dtype)  # float64
-int_arr = double_arr.astype('i')  # convert to integer
+int_arr = double_arr.astype("i")  # convert to integer
 print(int_arr)  # [1 2 3 4]
+
+#  Diff between copy and view
+#  copied data owns the copy, view doesn't it will modify original source like shallow copy
+
+print(arr)
+
+new_arr = arr.copy()
+view_arr = arr.view()
+
+new_arr[0] = "621"
+view_arr[0] = "721"
+
+print(arr)
+print(new_arr)
+print(view_arr)
+
+#  shape
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+
+print(arr.shape)  # 2,1,4 means I have two boxes with 1X4 in each?
+print(arr.ndim)  # 2 dim
+
+streams_arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+
+result = [e * 2 for e in streams_arr]  # prints each element
+print(result)  # this will have scalar values in the ndarray
+print(streams_arr * 2)  # doubles each element in the array
+
+# the main diff between list and ndarray is that ndarray is more efficient in terms of memory and performance. It allows for vectorized operations, meaning you can perform operations on entire arrays without the need for explicit loops, which is not possible with standard Python lists.
+# allso list stores in objects and ndarray stores in contiguous memory locations which makes it more efficient for numerical computations.
+# plus , minus, multiplication, division and other operations are performed element-wise in ndarray, while in lists you would need to use loops or comprehensions to achieve similar results.
