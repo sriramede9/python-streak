526fa68104e4bd08a8762aac15f6917850602da1
sriramede9 <only4programming03@gmail.com>
2026-07-19 09:43:12 -0400

    python: inheritence in python

--- sales-analysis/play_with_classes.py
+++ sales-analysis/play_with_classes.py
@@ -2,3 +2,36 @@ class Dog:
     def __init__(self, name, breed):
         self.name = name
         self.breed = breed
+
+class Cat:
+    def __init__(self,name,breed):
+        self.name = name
+        self.breed = breed
+
+
+mydog = Dog(name="D1",breed="b1")
+
+print(mydog.name,mydog.breed)
+
+class Animal:
+    def __init__(self,name,breed):
+        self.name = name
+        self.breed = breed
+        pass
+
+    def eat(self):
+        print(f"{self.name}, is eating")
+
+    def sleep(self):
+        print(f"{self.name}, is sleeping")
+    
+class Rhino(Animal):
+    def run(self):
+        print(f"Rhiono {self.name} can run")
+
+
+mini_rhino = Rhino(name="R1",breed="The whity")
+
+mini_rhino.eat()
+mini_rhino.sleep()
+mini_rhino.run()
